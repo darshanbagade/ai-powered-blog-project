7911b0b2040c6d9b86f31e802d693cfb8d523bdf
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-03 23:22:42 +0530

    feat: Implement addBlog function for creating blog posts with image upload

---
+++ server/controllers/blogController.js
@@ -0,0 +1,59 @@
+import {Blog} from '../models/blog.model.js';
+import fs from 'fs';
+import getImageKit from '../configs/imagekit.js';
+
+export const addBlog = async (req, res) => {
+    try {
+        
+        const {title,subTitle , description, category, isPublished} = JSON.parse(req.body.blog)
+        const imageFile = req.file;
+
+        
+        if(!title || !description || !category || !imageFile ){
+            return res.json({success : false , message: " Missing required fields"})
+        }
+
+        
+        //convert image to base64
+        const bufferFile  = fs.readFileSync(imageFile.path)
+
+
+        // Get ImageKit instance when needed (after env is loaded)
+        const imagekit = getImageKit();
+
+        //upload image to imagekit.io
+        const response = await imagekit.upload({
+            file : bufferFile,
+            fileName : imageFile.originalname,
+            folder : "/blogs"
+        })
+        console.log(response)
+
+        //optimization of image through imagekit transformation
+        const optimizatedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation :[
+                {quality : 'auto'}, //auto compression
+                {format : 'webp'},  //convert to modern format            ]
+                { width: '1280'}    // width resixing
+            ]
+        })
+
+        const image = optimizatedImageUrl;
+
+        const blogData = {
+            title,
+            subTitle,
+            description, 
+            category, 
+            image, 
+            isPublished : isPublished === "true" || isPublished === true
+        }
+        await Blog.create(blogData)
+
+        res.json({success : true , message: " blog added successfully"})
+
+    } catch (error) {
+        return res.json({success : false , message : error.message})
+    }
+}
