25ea786e90cc2d81ffbab6d897f1fd2612874a37
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-04 15:35:26 +0530

    feat: Update blog router to include additional routes for blog management

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { addBlog, getBlogList } from '../controllers/blogController.js';
+import { addBlog, addCommment, deleteBlogById, getBlogById, getBlogComments, getBlogList, togglePublish } from '../controllers/blogController.js';
 import { auth } from '../middleware/auth.middleware.js';
 import upload from '../middleware/multter.middleware.js'
 
@@ -7,6 +7,12 @@ const blogRouter  = Router();
 
 blogRouter.post('/add',upload.single('image') , auth, addBlog);
 blogRouter.get('/all',getBlogList)
+blogRouter.get('/:blogId',getBlogById)
+blogRouter.post('/delete',auth , deleteBlogById)
+blogRouter.post('/toggle-publish',auth , togglePublish)
 
+
+blogRouter.post('/add-comment',addCommment)
+blogRouter.get('/comments',getBlogComments)
 
 export default blogRouter
