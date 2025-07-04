772a4fbed2e93be0df31f5e593a3f6d17b038a9a
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-04 15:35:18 +0530

    feat: Enhance admin router with additional routes for blog and comment management

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -1,8 +1,14 @@
-import { adminController } from "../controllers/adminController.js";
+import { getAllBlogAdmin, adminController, approveCommentById, deleteCommentById, getAllComments, getDashboardData } from "../controllers/adminController.js";
 import { Router } from "express";
-
+import { auth } from "../middleware/auth.middleware.js";
 const adminRouter = Router();
 
 adminRouter.post('/login',adminController);
+adminRouter.get('/blogs',auth, getAllBlogAdmin)
+adminRouter.get('/comments',auth, getAllComments)
+adminRouter.post('/approve-comment',auth, approveCommentById)
+adminRouter.post('/delete-comment',auth, deleteCommentById)
+adminRouter.get('/dashboard',auth, getDashboardData)
+
 
 export default adminRouter
