e1648ad7cd76751ee51dc4671ebad5c365830e1d
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-05 22:49:36 +0530

    feat: Integrate AppProvider for context management and update admin route logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,23 @@ import {Routes, Route} from 'react-router-dom'
 import {Home, Blog, Dashboard, ListBlog, AddBlog, Layout,Comments} from './pages/index'
 import {Login} from './components/index'
 import 'quill/dist/quill.snow.css'
-
+import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
 
 function App() {
+const {token} = useAppContext()
+
   return (
     <div>
+      <Toaster/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/blog/:id' element={<Blog/>}/>
-        <Route path='/admin' element={ true ? <Layout/> : <Login/>}>
+        <Route path='/admin' element={ token ? <Layout/> : <Login/>}>
           <Route index element={<Dashboard/>} />
           <Route path='/admin/list-blog' element={<ListBlog/>} /> 
           <Route path='/admin/comments' element={<Comments/>} /> 
           <Route path='/admin/add-blog' element={<AddBlog/>} /> 
-
         </Route>
       </Routes>
     </div>
