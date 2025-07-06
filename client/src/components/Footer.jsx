cab159719f686b26e37d02d065c7dc659c9a26af
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-06 18:05:19 +0530

    feat: Update favicon and logo formats, and modify footer copyright text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
           </div >
 
         </div>
-        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuickBlog GreatStack - All Right Reserved.</p>
+        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 © QuillMind - All Right Reserved.</p>
     </div>
   )
 }
