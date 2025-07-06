51839789a1e430103594df9e13c8079e5b11d58e
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-06 14:29:06 +0530

    feat: Add approve and delete functionality to CommentsTableItem component

--- client/src/components/admin/CommentsTableItem.jsx
+++ client/src/components/admin/CommentsTableItem.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 function CommentsTableItem({comment, fetchComments}) {
   
     const { blog, createdAt , _id, name , content, isApproved } = comment;
     const date = new Date(createdAt);
+
+    const {axios} = useAppContext()
+    const handleDelete = async ()=>{
+        try {
+            const {data} = await axios.post("/api/admin/delete-comment",{id:_id}) 
+            if(data.success){
+                toast.success(data.message);
+                fetchComments();
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    const handleApprove = async ()=>{
+        try {
+            const {data} = await axios.post("/api/admin/approve-comment",{id:_id}) 
+            if(data.success){
+                toast.success(data.message);
+                fetchComments();
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
    return (
     <>
@@ -24,9 +56,15 @@ function CommentsTableItem({comment, fetchComments}) {
                 <div   className='inline-flex items-center gap-4'>
                     { 
 
-                        !isApproved ? <img src={assets.tick_icon} className=' w-5 hover:scale-110 transition-all cursor-pointer' /> : <p className='text-xs border border-green-600 bg-green-100 text-green-600 rounded-full px-3 py-1'>Approved</p>
+                        !isApproved ? <img src={assets.tick_icon} className=' w-5 hover:scale-110 transition-all cursor-pointer' 
+                        onClick={handleApprove}
+                        /> : <p className='text-xs border border-green-600 bg-green-100 text-green-600 rounded-full px-3 py-1'>Approved</p>
                     }                    
-                    <img src={assets.bin_icon} className="w-5 hover:scale-110 transition-all cursor-pointer" />
+                    <img 
+                        src={assets.bin_icon} 
+                        className="w-5 hover:scale-110 transition-all cursor-pointer" 
+                        onClick={handleDelete}
+                        />
                 </div>
             </td>
         </tr>
