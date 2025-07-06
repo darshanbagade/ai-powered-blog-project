2b9e21693eac9d801376f402834737971d61472f
Darshan Bagade <darshanbagade123@gmail.com>
2025-07-06 14:28:59 +0530

    feat: Enhance BlogTable component with publish and delete functionality

--- client/src/components/admin/BlogTable.jsx
+++ client/src/components/admin/BlogTable.jsx
@@ -1,12 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../../assets/assets';
-
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 function BlogTable({blog,fetchBlogs, index}) {
   
     const { title ,createdAt } = blog;
-
+    // console.log(blog);
+    
     //Date() Class is used to apply the formatting on the Dates as Date comes in string format from the APIs
     const BlogDate = new Date(createdAt) 
+    // console.log(BlogDate)
+    const {axios} = useAppContext();
+    const handlePublish = async()=>{
+        try{
+            const response = await axios.post('api/admin/toggle-publish',{id:blog._id})
+            if(response.success){
+                // toast.success(response.message)
+                // console.log(response.message);
+                await fetchBlogs();
+            }else{
+                // toast.success(response.message)/
+            }
+        }catch(error){
+            toast.error(error.message)
+        }
+    }
+
+    const handleDelete = async ()=>{
+        try{
+            const{ data } = await axios.post('/api/admin/delete',{id:blog._id})
+            if(data.success){
+                toast.success(data.message);
+            }else{
+                toast.error(data.message)
+            }
+            await fetchBlogs()
+        }catch(error){
+            toast.error(error.message)
+        }
+    }
+
     return (
     <tr className='border-y border-gray-300 '>
         <th className='px-2 py-4 '>{index}</th>
@@ -18,8 +51,14 @@ function BlogTable({blog,fetchBlogs, index}) {
             >{blog.isPublished ? 'Published' : 'Unpublished'}</p>
         </td>
         <td className='px-2 py-4 flex text-xs gap-3'>
-            <button className='border px-2 py-0.5 mt-1 rounded cursor-pointer'>{blog.isPublished ? 'Published' : 'Unpublished'}</button>
-            <img src={assets.cross_icon} className="w-8 hover:scale-110 transition-all cursor-pointer" />
+            <button className='border px-2 py-0.5 mt-1 rounded cursor-pointer'
+                onClick={handlePublish}
+            >{blog.isPublished ? 'Unpublish' : 'Publish'}</button>
+            <img 
+                src={assets.cross_icon} 
+                className="w-8 hover:scale-110 transition-all cursor-pointer" 
+                onClick={handleDelete}
+            />
         </td>
     </tr>
   )
